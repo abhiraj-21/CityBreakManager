22e80ea907be832305806ff81c89025dc82be7e6
abhiraj-21 <abhirajsinghchouhan4@gmail.com>
2025-12-25 11:29:49 +0530

    initial project structure

---
+++ city-break-manager/src/test/java/com/abhiraj/citybreakmanager/CityBreakManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiraj.citybreakmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBreakManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
